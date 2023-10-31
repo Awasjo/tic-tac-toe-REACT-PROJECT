28d227298f423b3436908cff7548b2f2c04aa365
Awasjo <awasjomail@gmail.com>
2023-10-30 20:16:18 -0400

    added components and learned lift state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import { useState } from "react";
+
 function App() {
+const[activePlayer, setActivePlayer]=useState('X');
+
+function handleSelectedSquare(){
+  setActivePlayer((curActivePlayter)=> curActivePlayter==='X'? 'O' : 'X')
+}
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <li>
-            <span className="player-name">Player1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player2</span>
-            <span className="player-symbol">O</span>
-          </li>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player1" symbol="X" isActive={activePlayer ==='X'}/>
+          <Player initialName="Player2" symbol="O" isActive={activePlayer ==='O'}/>
         </ol>
+          <GameBoard onSelectSquare={handleSelectedSquare} activePlayerSymbol={activePlayer}/>
       </div>
+      Log
     </main>
   );
 }
---
+++ src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol,isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  //if you want to update a state based on the previous state its best practice to pass a function rather than a !isEditing
+  //if we did it the non best practice way, react will schedule it to change the state, with a function its change right away.
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    console.log(isEditing)
+  }
+
+  //the object event is describing the event that happened when the change even occurs
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value); //this is the input element, and holds a value property
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    // on change prop will trigger for every key stroke and will give back an event object for every key stroke that the user hit
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive?'active' :undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
