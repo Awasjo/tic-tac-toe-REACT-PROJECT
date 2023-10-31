bd85d02dbdd7321d74b801d316f62ce7f71ff9eb
Awasjo <awasjomail@gmail.com>
2023-10-30 21:21:13 -0400

    implement logic to see which player is pressing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,70 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
+import Log from "./components/Log";
+
+//helper function
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer
+}
+
 
 function App() {
-const[activePlayer, setActivePlayer]=useState('X');
+  //const [activePlayer, setActivePlayer] = useState("X"); //this line is connected to the gameboard and player EJX files, this is lifting the state up which is a react concept
+  const [gameTurns, setGameTurns] = useState([]); //this is to be used for the log EJX file, where we want to add turns to the array
 
-function handleSelectedSquare(){
-  setActivePlayer((curActivePlayter)=> curActivePlayter==='X'? 'O' : 'X')
-}
+  //instead of having an active player state which i commented out above, we can add derived state written below 
+  const activePlayer = deriveActivePlayer(gameTurns)
+
+  function handleSelectedSquare(rowIndex, colIndex) {
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    //this is a state updating function below, that is updating the project on who has the latest turn and the symbol they are using
+    setGameTurns((prevTurns) => {
+
+      const currentPlayer = deriveActivePlayer(prevTurns)
+
+      // let currentPlayer = "X";
+
+      // //this is if the latest turn checker
+      // if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+      //   currentPlayer = "O";
+      // }
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: activePlayer }, //we are creating an object here, which has two properties
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player1" symbol="X" isActive={activePlayer ==='X'}/>
-          <Player initialName="Player2" symbol="O" isActive={activePlayer ==='O'}/>
+          <Player
+            initialName="Player1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-          <GameBoard onSelectSquare={handleSelectedSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard
+          onSelectSquare={handleSelectedSquare}
+          // activePlayerSymbol={activePlayer}
+          turns={gameTurns}
+        />
       </div>
-      Log
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 const initialGameBoard = [
   [null, null, null],
@@ -6,28 +6,46 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard)
-  
-function handleSelectSquare(rowIndex, colIndex){
-    setGameBoard((prevGameBoard)=>{
-        //updating the state in an immutable way which is recommended by react 
-        const updatedBoard =[...prevGameBoard.map(innerArray=>[...innerArray])];
-        updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-        return updatedBoard
-    })
+//the change that we want TODO here is to lift up the game board information so that we can use it in the log ejx, refer to the app component
 
-   onSelectSquare(); 
-}
+export default function GameBoard({
+  onSelectSquare /*activePlayerSymbol*/,
+  turns,
+}) {
+  //initial game board
+  let gameBoard = initialGameBoard;
 
-    return (
+  for (const turn of turns) {
+    //want to extract the info from the turn that just occured
+    const { square, player } = turn; //these are two properrties from the app component
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  //   const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+  // function handleSelectSquare(rowIndex, colIndex){
+  //     setGameBoard((prevGameBoard)=>{
+  //         //updating the state in an immutable way which is recommended by react
+  //         const updatedBoard =[...prevGameBoard.map(innerArray=>[...innerArray])];
+  //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //         return updatedBoard
+  //     })
+
+  //    onSelectSquare();
+  // }
+
+  return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={()=> handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
